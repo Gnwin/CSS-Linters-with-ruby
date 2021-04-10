849230af6051da5bd3931b9d03deb55799633150
Godwin Nwachukwu <nwgwin@gmail.com>
2021-04-10 15:14:37 +0100

    write seci=ond tests for User class

--- spec/user_file_spec.rb
+++ spec/user_file_spec.rb
@@ -1,14 +1,29 @@
 require './lib/user_file'
 
 RSpec.describe User do
-  let(:file_array){ ["sdjfjf", "reriii", "fnfjfjjk"] }
+  let(:file_path) { "files/script.js" }
 
   describe '#read_file' do
-    subject { User.new(file_array.to_s) }
+    subject { User.new(file_path) }
+
     context 'when the linters start running' do
       it 'reads the user file from the file path' do
         expect(subject.read_file.class).to eql(Array)
       end
     end
+
+    context 'when the linters start running' do
+      it 'reads the user file from the file path' do
+        arr = subject.read_file
+        expect(arr.all?(String)).to eql(true)
+      end
+    end
+
+    # context 'when the linters start running' do
+    #   it 'reads the user file from the file path' do
+        
+    #     expect(subject.read_file[0].split.include?('\n')).to eql(true)
+    #   end
+    # end
   end 
 end
