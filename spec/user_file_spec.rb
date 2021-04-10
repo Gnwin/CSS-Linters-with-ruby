5822998c2a13ce4da23e99f5b5e4bab6faa816c2
Godwin Nwachukwu <nwgwin@gmail.com>
2021-04-10 14:52:10 +0100

    write rspec tests fo User class

--- spec/user_file_spec.rb
+++ spec/user_file_spec.rb
@@ -1,1 +1,14 @@
 require './lib/user_file'
+
+RSpec.describe User do
+  let(:file_array){ ["sdjfjf", "reriii", "fnfjfjjk"] }
+
+  describe '#read_file' do
+    subject { User.new(file_array.to_s) }
+    context 'when the linters start running' do
+      it 'reads the user file from the file path' do
+        expect(subject.read_file.class).to eql(Array)
+      end
+    end
+  end 
+end
